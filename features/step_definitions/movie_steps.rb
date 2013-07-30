0f7cc03cec25c27da680411474b53e7b5d675fe8
Eric Lethier <e.lethier@gmail.com>
2013-07-30 05:57:27 -0700

    Added cucumber test to test filter feature

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +25,14 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_array = rating_list.split(',')
+  if uncheck
+    rating_array.each do |rating|
+      uncheck("ratings_" + rating.strip)
+    end
+  else
+    rating_array.each do |rating|
+      check("ratings_" + rating.strip)
+    end
+  end
 end
