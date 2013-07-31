ffa02240f48497878fe50d29ff55b38342df2e5f
Eric Lethier <e.lethier@gmail.com>
2013-07-31 01:20:28 -0700

    Added cucumber test to test the sort feature

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,14 +14,19 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  is_before = page.body.delete("\n").match(/(#{e1}).*(#{e2})/)
+  is_before.nil?.should == false
 end
 
+# Check if all the movies are displayed
 Then /I should see all of the movies/ do
   rows = page.all('#movies tr').size
   rows.should == 11
 end
 
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
 Given /I check the following ratings: (.*)/ do |rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
